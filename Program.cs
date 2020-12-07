5023d57be5741a692f0ac32bc91d3fa62ee3a5a3
marcello <marcello>
2020-12-07 11:22:55 +0100

    terminati test

--- Program.cs
+++ Program.cs
@@ -19,15 +19,31 @@ namespace ranx
 
             var config = builder.Build();
 
-            Console.WriteLine("Test addRowTotal");
-            TestAddRowTotal(config);
+            BuildMenu();
+            //TestBuildDataForPivot(config);
+            //TestAddRowTotal(config);
+            //TestBuildPivot(config);
+            RunAll(config);
             
             Console.ReadLine();
 
         }
 
+        public static void BuildMenu()
+        {
+           
+        }
+
+        public static void RunAll(IConfigurationRoot config)
+        {
+            TestAddRowTotal(config);
+            //TestBuildPivot(config);
+            TestPivotAtOnce(config);
+        }
+
         public static void TestAddRowTotal(IConfigurationRoot config)
-        {     
+        {
+            Console.WriteLine("Test addRowTotal");
             string sql = @"SELECT TOP 10
                         ProductName
                           ,sum(quantity) as Bottles_sold
@@ -37,14 +53,75 @@ namespace ranx
             var results = GetDataTable(config, sql, null);
             
             var file = AppDomain.CurrentDomain.BaseDirectory + "/output/rowtotal.xlsx";
-            ExcelHelper eh = ExcelHelper.Create(file);
-            eh.Dump(results, "SHEET1");
-            eh.AddRowTotal("SHEET1");
-            //eh.DoPivotTable("SHEET1", "SHEET2", new string[]{"ProductName"}, new string[] {"Subtotal_per_Wine" });
-            eh.Save();
+            if (System.IO.File.Exists(file))
+            {
+                System.IO.File.Delete(file);
+            }
+            ExcelHelper eh = ExcelHelper.Create(file)
+                        .Dump(results, "SHEET1")
+                        //.DoPivotTable("SHEET1","SHEET2", new string[]{"ProductName"}, new string[]{"Bottles_sold"}, new string[]{"Subtotal_per_Wine" })
+                        .AddRowTotal("SHEET1")
+                        .Save();
             eh.Dispose();
             Console.WriteLine("file created in " + file);
             Console.WriteLine("End testAddRowTotal");
+        }
+
+        public static void TestBuildDataForPivot(IConfigurationRoot config)
+        {
+            string sql = "SELECT * FROM Invoices ORDER BY OrderDate";
+            var results = GetDataTable(config, sql, null);
+            var file = AppDomain.CurrentDomain.BaseDirectory + "/output/pivot.xlsx";
+            ExcelHelper eh = ExcelHelper.Create(file)
+                        .Dump(results, "SHEET1")
+                        .Save();
+            eh.Dispose();
+        }
+
+        public static void TestBuildPivot(IConfigurationRoot config)
+        {
+            Console.WriteLine("Begin TestBuildPivot");
+            var file = AppDomain.CurrentDomain.BaseDirectory + "/output/pivot.xlsx";
+            if (System.IO.File.Exists(file) != true)
+            {
+                TestBuildDataForPivot(config);
+            }
+            ExcelHelper eh = ExcelHelper.Create(file)
+                                        .DoPivotTable("SHEET1", 
+                                                        "SHEET2", 
+                                                        new string[] { "ShipCountry" },
+                                                        new string[] { "ProductName" },
+                                                        new string[] { "ExtendedPrice" }
+                                                     )
+                                        .Save();
+            eh.Dispose();
+            Console.WriteLine("file created in " + file);
+            Console.WriteLine("End testBuildPivot");
+
+        }
+
+        public static void TestPivotAtOnce(IConfigurationRoot config)
+        {
+            Console.WriteLine("Begin TestPivotAtOnce");
+            string sql = "SELECT * FROM Invoices ORDER BY OrderDate";
+            var results = GetDataTable(config, sql, null);
+            var file = AppDomain.CurrentDomain.BaseDirectory + "/output/pivot_at_once.xlsx";
+            if (System.IO.File.Exists(file))
+            {
+                System.IO.File.Delete(file);
+            }
+            ExcelHelper eh = ExcelHelper.Create(file)
+                        .Dump(results, "SHEET1")
+                        .DoPivotTable("SHEET1",
+                                                        "SHEET2",
+                                                        new string[] { "ShipCountry" },
+                                                        new string[] { "ProductName" },
+                                                        new string[] { "ExtendedPrice" }
+                                                     )
+                        .Save();
+            eh.Dispose();
+            Console.WriteLine("file created in " + file);
+            Console.WriteLine("End testPivotAtOnce");
         }
 
         public static DataTable GetDataTable(IConfigurationRoot config, string sql, Hashtable _params = null)
